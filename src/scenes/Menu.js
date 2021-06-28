efcfdbd722b43464a952a94a4d5af741b243337e
Alex Phun <alphun@ucsc.edu>
2021-06-27 17:23:32 -0700

    text test

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,7 +9,9 @@ class Menu extends Phaser.Scene {
     preload() {
         // load audio
         this.load.audio('sfx_select', './assets/blip_select12.wav');
+
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
+
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
     create() {
@@ -23,6 +25,7 @@ class Menu extends Phaser.Scene {
                 top: 5, bottom: 5
             },
             fixedWidth: 0
+            
         }
         this.add.text(game.config.width/2, game.config.height/2 - (borderUISize + borderPadding), 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
